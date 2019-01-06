6cd19c2b8c8c3b4ed16066e7269a1373b0d7ba0d
boisterous-design-4553 <reemimhussein@outlook.com>
2019-01-06 21:14:00 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,35 +1,35 @@
 def addition(num1=5, num2=4)
-  sum==#{num1}+#{num2}
+  sum=#{num1}+#{num2}
   puts sum
 end
 
 def subtraction(num1=10, num2=5)
-  subtraction==num1-num2
+  subtraction=#{num1}-{num2}
   puts subtraction
 end
 
 def division(num1=50, num2=2)
-  quotient==num1/num2
+  quotient=#{num1}/{num2}
   puts quotient
 end
 
 def multiplication(num1=4, num2=30)
-  product==num1*num2
+  product=#{num1}*{num2}
   puts product
 end
 
 def modulo(num1=34, num2=5)
-  modulo==num1 % num2
+  modulo=#{num1} % #{num2}
   puts modulo
   
 end
 
 def square_root(num=81)
-  response == Math.sqrt(num)
+  response = Math.sqrt#{num}
   puts response
 end
 
 def order_of_operation(num1=7, num2=43, num3=23, num4=83)
-  order_of_operation==num1 + (( num2 * num3 ) / num4)
+  order_of_operation=num1 + (( #{num2} * #{num3} ) / #{num4})
   puts order_of_operation
 end
